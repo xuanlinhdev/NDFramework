52227178e871c901ff1dba8341e2d4febb0e188b
xuanlinhdev <111721696+xuanlinhdev@users.noreply.github.com>
2023-03-20 11:36:22 +0700

    Update

---
+++ NDFramework/HelloWorld.swift
@@ -0,0 +1,22 @@
+//
+//  HelloWorld.swift
+//  Sample Framework
+//
+//  Created by Xuân Linh on 17/03/2023.
+//
+
+import Foundation
+
+public class HelloWorld{
+    public init(){
+        
+    }
+    
+    public func printMessage(mess:String){
+        print(mess)
+    }
+    
+    public func Sum(valueX: Int,valueY:Int) -> Int{
+        return valueX*valueY
+    }
+}
