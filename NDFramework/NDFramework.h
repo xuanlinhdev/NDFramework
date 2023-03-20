385562bc697c6643a6d247fc52461b1d91b2fc28
xuanlinhdev <111721696+xuanlinhdev@users.noreply.github.com>
2023-03-20 10:06:45 +0700

    Initial Commit

---
+++ NDFramework/NDFramework.h
@@ -0,0 +1,18 @@
+//
+//  NDFramework.h
+//  NDFramework
+//
+//  Created by Xuân Linh on 20/03/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NDFramework.
+FOUNDATION_EXPORT double NDFrameworkVersionNumber;
+
+//! Project version string for NDFramework.
+FOUNDATION_EXPORT const unsigned char NDFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NDFramework/PublicHeader.h>
+
+
