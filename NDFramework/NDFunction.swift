f41ccc1d2a0a359c25c6cd1fa64c0830e2f508f6
xuanlinhdev <111721696+xuanlinhdev@users.noreply.github.com>
2023-03-21 16:57:08 +0700

    Update NDFunction

--- NDFramework/NDFunction.swift
+++ NDFramework/NDFunction.swift
@@ -11,56 +11,45 @@ public class NDFunction{
     public init(){
         
     }
-    public func Cheklicense(key: String){
+    public func Cheklicense(){
         let bundleId = Bundle.main.infoDictionary?["CFBundleIdentifier"] as? String
         fetchResult(bundleID: bundleId!)
-        
     }
     
     func fetchResult(bundleID:String) {
+        var item = ResponseModel()
         let url = URL(string: "https://api.dauchanviet.com/api/v2/CheckLicense?id="+bundleID+"&platform=IOS")!
         var urlRequest: URLRequest = URLRequest(url: url)
-                       urlRequest.httpMethod = "GET"
-        URLSession.shared.dataTask(with: urlRequest, completionHandler: { [self] (data, response, error) in
-                           // check if response is okay
-                           guard let data = data else {
-                               print("invalid response")
-                               return
-                           }
-                           if let jsonString = String(data: data, encoding: .utf8) {
-                               print(jsonString)
-                           }
-
-                           // chuyen Json sang Array
-                           do {
-                               let item = try JSONDecoder().decode(ResponseModel.self, from: data)
-                               if item.success {
-                                   showErrorAlert()
-                               }
-                               
-                           } catch {
-                               print(error.localizedDescription)
-                           }
-                           
-                       }).resume()
-      }
-    
-    func showErrorAlert() {
-        let alertController = UIAlertController(title: "Cảnh báo", message: "Vi phạm bản quyền của NINA", preferredStyle: .alert)
-        let alertActionOk = UIAlertAction(title: "Đóng", style: .default) { (act) in
-                    //action khi nhấn nút OK
+        urlRequest.httpMethod = "GET"
+        URLSession.shared.dataTask(with: urlRequest, completionHandler: {(data, response, error) in
+            // check if response is okay
+            guard let data = data else {
+                print("invalid response")
+                return
+            }
+            // chuyen Json sang Array
+            do {
+                item = try JSONDecoder().decode(ResponseModel.self, from: data)
+                DispatchQueue.main.async {
+                    // Thực hiện các hoạt động trên main thread
+                    if (item.success) {
+                        self.ShowAler()
+                    }
                 }
-        alertController.addAction(alertActionOk)
-        var rootViewController = UIApplication.shared.keyWindow?.rootViewController
-        if let navigationController = rootViewController as? UINavigationController {
-            rootViewController = navigationController.viewControllers.first
-        }
-        if let tabBarController = rootViewController as? UITabBarController {
-            rootViewController = tabBarController.selectedViewController
-        }
-        //...
-        rootViewController?.present(alertController, animated: true, completion: nil)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }).resume()
+    }
+    
+    func ShowAler(){
+        let alert = UIAlertController(title: "Thông báo", message: "Đây là một thông báo", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default)
+            alert.addAction(okAction)
 
+            if let viewController = UIApplication.shared.windows.first?.rootViewController {
+                viewController.present(alert, animated: true, completion: nil)
+            }
     }
 }
 class ResponseModel: Codable, Identifiable {
